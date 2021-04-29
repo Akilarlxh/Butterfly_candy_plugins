023eac5ed4e5bc3617389330fc210864acb76ba8
akilarlxh <akilarlxh@gmail.com>
2021-04-29 10:30:52 +0800

    阿尔法测试通过

--- hexo-butterfly-fcircle/assets/fcircle.js
+++ hexo-butterfly-fcircle/assets/fcircle.js
@@ -83,6 +83,7 @@ var data_handle = (nofollow, data, maxnumber) => {
             html_item += '</div>';
           html_item += '</div>';
         html_item += '</div>';
+        html_item += '<hr>';
   //------统计表结束-------
 
   for (var month_item of datalist_slice) {
@@ -127,7 +128,7 @@ var data_handle = (nofollow, data, maxnumber) => {
   }
   //加载更多
   if (data[1].length - maxnumber > 0) {
-    html_item += '<div class="fc_center"><button type="button" class="moments_load_button" onclick="load_more_post()">加载更多...</button></div>'
+    html_item += '<div style="text-align: center;"><button type="button" class="moments_load_button" onclick="load_more_post()">加载更多...</button></div>'
   }
 
   var friend_link_circle = document.getElementById('friend_link_circle');
